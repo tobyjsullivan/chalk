48fb6dcea805696cd279297b5f05bd7d4ff51c4f
Toby Sullivan <tobyjsullivan@gmail.com>
2018-10-01 17:21:39 +1000

    Implement a concatenate function

--- lib/std/concatenate.go
+++ lib/std/concatenate.go
@@ -7,21 +7,21 @@ import (
 	"github.com/tobyjsullivan/chalk/types"
 )
 
-var Sum = &functions.Function{
+var Concatenate = &functions.Function{
+	Parameters: []types.Type{types.TString, types.TString},
+	Variadic:   true,
+	Returns:    types.TString,
 	Handler: func(params ...types.Object) (types.Object, error) {
-		var acc float64
+		var acc string
 		for i, p := range params {
-			cur, err := p.AsNumber()
+			s, err := p.AsString()
 			if err != nil {
 				return nil, errors.New(fmt.Sprintf("unexpected param type %d: %s", i, err))
 			}
 
-			acc += cur.Raw()
+			acc += s.Raw()
 		}
 
-		return types.NewNumber(acc), nil
+		return types.NewString(acc), nil
 	},
-	Parameters: []types.Type{types.TNumber, types.TNumber},
-	Variadic:   true,
-	Returns:    types.TNumber,
 }
---
+++ lib/std/concatenate_test.go
@@ -0,0 +1,25 @@
+package std
+
+import (
+	"github.com/tobyjsullivan/chalk/types"
+	"testing"
+)
+
+func TestConcatenate_Handler(t *testing.T) {
+	result, err := Concatenate.Handler(
+		types.NewString("Hello"),
+		types.NewString(", "),
+		types.NewString("World!"))
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	s, err := result.AsString()
+	if err != nil {
+		t.Errorf("Unexpected cast error: %v", err)
+	}
+
+	if raw := s.Raw(); raw != "Hello, World!" {
+		t.Errorf("Unexpected result value: %s", raw)
+	}
+}
