01113028a855d9a5a8425697b3a4e6efd1bd6e1b
Toby Sullivan <tobyjsullivan@gmail.com>
2019-02-24 22:56:43 +1100

    Drop private subnet

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -99,7 +99,7 @@ resource "aws_ecs_service" "main" {
 
   network_configuration {
     security_groups = ["${aws_security_group.ecs_service.id}"]
-    subnets = ["${aws_subnet.private_subnet.*.id}"]
+    subnets = ["${aws_subnet.public_subnet.*.id}"]
     assign_public_ip = true
   }
 }
@@ -136,31 +136,12 @@ resource "aws_vpc" "main" {
   enable_dns_support   = true
 }
 
-/*
-From the docs:
-Only private subnets are supported for the awsvpc network mode. Because tasks do not receive public IP
-addresses, a NAT gateway is required for outbound internet access. Inbound internet traffic should be
-routed through a load balancer.
- */
-
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = "${aws_vpc.main.id}"
   count                   = "${length(var.public_subnets_cidr)}"
   cidr_block              = "${element(var.public_subnets_cidr, count.index)}"
   availability_zone       = "${element(var.availability_zones, count.index)}"
   map_public_ip_on_launch = true
-}
-
-resource "aws_subnet" "private_subnet" {
-  vpc_id                  = "${aws_vpc.main.id}"
-  count                   = "${length(var.private_subnets_cidr)}"
-  cidr_block              = "${element(var.private_subnets_cidr, count.index)}"
-  availability_zone       = "${element(var.availability_zones, count.index)}"
-  map_public_ip_on_launch = false
-}
-
-resource "aws_route_table" "private" {
-  vpc_id = "${aws_vpc.main.id}"
 }
 
 resource "aws_route_table" "public" {
@@ -173,37 +154,14 @@ resource "aws_route" "public_internet_gateway" {
   gateway_id             = "${aws_internet_gateway.ig.id}"
 }
 
-resource "aws_route" "private_nat_gateway" {
-  route_table_id         = "${aws_route_table.private.id}"
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.nat.id}"
-}
-
 resource "aws_route_table_association" "public" {
   count          = "${length(var.public_subnets_cidr)}"
   subnet_id      = "${element(aws_subnet.public_subnet.*.id, count.index)}"
   route_table_id = "${aws_route_table.public.id}"
 }
 
-resource "aws_route_table_association" "private" {
-  count           = "${length(var.private_subnets_cidr)}"
-  subnet_id       = "${element(aws_subnet.private_subnet.*.id, count.index)}"
-  route_table_id  = "${aws_route_table.private.id}"
-}
-
 resource "aws_internet_gateway" "ig" {
   vpc_id = "${aws_vpc.main.id}"
-}
-
-resource "aws_eip" "nat_eip" {
-  vpc        = true
-  depends_on = ["aws_internet_gateway.ig"]
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = "${aws_eip.nat_eip.id}"
-  subnet_id     = "${element(aws_subnet.public_subnet.*.id, 0)}"
-  depends_on    = ["aws_internet_gateway.ig"]
 }
 
 /*
