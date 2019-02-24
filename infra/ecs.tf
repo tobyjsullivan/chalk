c728ba3b5fd19279ba449378ab6c430dc0e9f012
Toby Sullivan <tobyjsullivan@gmail.com>
2019-02-24 22:49:56 +1100

    WIP: get containers running on ECS

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -1,29 +1,27 @@
 data "aws_availability_zones" "available" {}
 
-//resource "aws_vpc" "main" {
-//  cidr_block = "10.10.0.0/16"
-//}
-//
-//resource "aws_subnet" "main" {
-//  count = 2
-//  cidr_block = "${cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)}"
-//  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
-//  vpc_id = "${aws_vpc.main.id}"
-//}
-//
-//resource "aws_security_group" "allow_all" {
-//  name        = "allow_all_a_1"
-//  description = "Allow all inbound traffic"
-//  vpc_id      = "${aws_vpc.main.id}"
-//
-//  ingress {
-//    protocol = "6"
-//    from_port = 80
-//    to_port = 8080
-//    cidr_blocks = ["${aws_vpc.main.cidr_block}"]
-//  }
-//}
+variable "vpc_cidr" {
+  default = "10.10.0.0/16"
+}
 
+variable "public_subnets_cidr" {
+  type = "list"
+  default = ["10.10.0.0/24"]
+}
+
+variable "private_subnets_cidr" {
+  type = "list"
+  default = ["10.10.1.0/24"]
+}
+
+variable "availability_zones" {
+  type = "list"
+  default = ["ap-southeast-2a"]
+}
+
+/*
+ * Docker image repositories
+ */
 resource "aws_ecr_repository" "monolith_svc" {
   name = "chalk-monolith-svc"
 }
@@ -36,39 +34,232 @@ resource "aws_ecr_repository" "api" {
   name = "chalk-api"
 }
 
-//resource "aws_ecs_cluster" "main" {
-//  name = "chalk-cluster"
-//}
-//
-//resource "aws_ecs_task_definition" "chalk_api" {
-//  family = "chalk-api"
-//  network_mode = "awsvpc"
-//  requires_compatibilities = ["FARGATE"]
-//  cpu = "256"
-//  memory = "512"
-//
-//  container_definitions = <<DEFINITION
-//[
-//  {
-//    "cpu": 256,
-//    "essential": true,
-//    "image": "mongo:latest",
-//    "memory": 512,
-//    "name": "mongodb",
-//    "networkMode": "awsvpc"
-//  }
-//]
-//DEFINITION
-//}
-//
-//resource "aws_ecs_service" "main" {
-//  name = "tf-ecs-service-1"
-//  cluster = "${aws_ecs_cluster.main.id}"
-//  task_definition = "${aws_ecs_task_definition.chalk_api.arn}"
-//  desired_count = 1
-//  launch_type = "FARGATE"
-//  network_configuration {
-//    security_groups = ["${aws_security_group.allow_all.id}"]
-//    subnets = ["${aws_subnet.main.*.id}"]
-//  }
-//}
+/*
+ * ECS Task definitions
+ */
+resource "aws_ecs_task_definition" "chalk_api" {
+  family = "chalk-api"
+  network_mode = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu = "256"
+  memory = "512"
+  execution_role_arn = "${aws_iam_role.ecs_execution_role.arn}"
+
+  container_definitions = <<DEFINITION
+[
+  {
+    "essential": true,
+    "image": "${aws_ecr_repository.monolith_svc.repository_url}",
+    "name": "monolith-svc",
+    "networkMode": "awsvpc",
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-group": "${aws_cloudwatch_log_group.chalk_services.name}",
+        "awslogs-region": "${data.aws_region.current.name}",
+        "awslogs-stream-prefix": "monolith-svc"
+      }
+    }
+  },
+  {
+    "essential": true,
+    "image": "${aws_ecr_repository.resolver_svc.repository_url}",
+    "name": "resolver-svc",
+    "networkMode": "awsvpc",
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-group": "${aws_cloudwatch_log_group.chalk_services.name}",
+        "awslogs-region": "${data.aws_region.current.name}",
+        "awslogs-stream-prefix": "repository-svc"
+      }
+    }
+  }
+]
+DEFINITION
+}
+
+/*
+ * ECS Fargate Cluster
+ */
+resource "aws_ecs_cluster" "main" {
+  name = "chalk-cluster"
+}
+
+/*
+ * Chalk Service
+ */
+resource "aws_ecs_service" "main" {
+  name = "chalk-backend-${var.env}-2"
+  cluster = "${aws_ecs_cluster.main.id}"
+  task_definition = "${aws_ecs_task_definition.chalk_api.arn}"
+  desired_count = 1
+  launch_type = "FARGATE"
+  depends_on = ["aws_iam_role_policy.ecs_execution_role_policy"]
+
+  network_configuration {
+    security_groups = ["${aws_security_group.ecs_service.id}"]
+    subnets = ["${aws_subnet.private_subnet.*.id}"]
+    assign_public_ip = true
+  }
+}
+
+resource "aws_security_group" "ecs_service" {
+  name        = "allow_all_a_1"
+  description = "Allow all inbound traffic"
+  vpc_id      = "${aws_vpc.main.id}"
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+/*
+ * The VPC
+ */
+resource "aws_vpc" "main" {
+  cidr_block = "${var.vpc_cidr}"
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+/*
+From the docs:
+Only private subnets are supported for the awsvpc network mode. Because tasks do not receive public IP
+addresses, a NAT gateway is required for outbound internet access. Inbound internet traffic should be
+routed through a load balancer.
+ */
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = "${aws_vpc.main.id}"
+  count                   = "${length(var.public_subnets_cidr)}"
+  cidr_block              = "${element(var.public_subnets_cidr, count.index)}"
+  availability_zone       = "${element(var.availability_zones, count.index)}"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = "${aws_vpc.main.id}"
+  count                   = "${length(var.private_subnets_cidr)}"
+  cidr_block              = "${element(var.private_subnets_cidr, count.index)}"
+  availability_zone       = "${element(var.availability_zones, count.index)}"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = "${aws_route_table.public.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.ig.id}"
+}
+
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = "${aws_route_table.private.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = "${aws_nat_gateway.nat.id}"
+}
+
+resource "aws_route_table_association" "public" {
+  count          = "${length(var.public_subnets_cidr)}"
+  subnet_id      = "${element(aws_subnet.public_subnet.*.id, count.index)}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_route_table_association" "private" {
+  count           = "${length(var.private_subnets_cidr)}"
+  subnet_id       = "${element(aws_subnet.private_subnet.*.id, count.index)}"
+  route_table_id  = "${aws_route_table.private.id}"
+}
+
+resource "aws_internet_gateway" "ig" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+resource "aws_eip" "nat_eip" {
+  vpc        = true
+  depends_on = ["aws_internet_gateway.ig"]
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = "${aws_eip.nat_eip.id}"
+  subnet_id     = "${element(aws_subnet.public_subnet.*.id, 0)}"
+  depends_on    = ["aws_internet_gateway.ig"]
+}
+
+/*
+ * Permissions Management
+ */
+resource "aws_iam_role" "ecs_execution_role" {
+  name_prefix  = "chalk-ecs_task_execution_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "ecs_execution_role_policy" {
+  name_prefix   = "chalk-ecs_execution_role_policy"
+  role   = "${aws_iam_role.ecs_execution_role.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+/*
+ * Observability
+ */
+resource "aws_cloudwatch_log_group" "chalk_services" {
+  name_prefix = "chalk-svcs_"
+
+  tags {
+    Environment = "${var.env}"
+    Application = "Chalk"
+  }
+}
